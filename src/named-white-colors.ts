6163b4cedf061e8e0dc81da5efb0463f2bc9bb0a
JADENgygo <29948746+JADENgygo@users.noreply.github.com>
2026-08-20 13:35:28 +0000

    実在する色名を白色パレットに適用

---
+++ src/named-white-colors.ts
@@ -0,0 +1,804 @@
+// color-name-list (MIT) からRGB各成分235以上の色を明るさ順に抽出。
+// このファイルは npm run generate:colors で再生成できます。
+export const namedWhiteColors = [
+  {
+    name: "White",
+    hex: "#ffffff",
+  },
+  {
+    name: "Whitewash",
+    hex: "#fefffc",
+  },
+  {
+    name: "Polar Bear in a Blizzard",
+    hex: "#fcffff",
+  },
+  {
+    name: "Ivory",
+    hex: "#fffff0",
+  },
+  {
+    name: "Ceramic",
+    hex: "#fcfff9",
+  },
+  {
+    name: "Springtime Dew",
+    hex: "#ffffef",
+  },
+  {
+    name: "Milk",
+    hex: "#fdfff5",
+  },
+  {
+    name: "Eburnean",
+    hex: "#ffffee",
+  },
+  {
+    name: "Baby Powder",
+    hex: "#fefefa",
+  },
+  {
+    name: "Vanilla Drop",
+    hex: "#ffffeb",
+  },
+  {
+    name: "Whitecap Snow",
+    hex: "#fffdfd",
+  },
+  {
+    name: "Pale Grey",
+    hex: "#fdfdfe",
+  },
+  {
+    name: "Luminary",
+    hex: "#fffeed",
+  },
+  {
+    name: "Touch of Mint",
+    hex: "#f8fff8",
+  },
+  {
+    name: "Brilliance",
+    hex: "#fdfdfd",
+  },
+  {
+    name: "Emptiness",
+    hex: "#fcfdfc",
+  },
+  {
+    name: "Pearl Powder",
+    hex: "#faffed",
+  },
+  {
+    name: "Granulated Sugar",
+    hex: "#fffdf2",
+  },
+  {
+    name: "Mint Cream",
+    hex: "#f5fffa",
+  },
+  {
+    name: "Sparkling Cider",
+    hex: "#fffdeb",
+  },
+  {
+    name: "Igloo",
+    hex: "#fdfcfa",
+  },
+  {
+    name: "Flawed White",
+    hex: "#fffbfc",
+  },
+  {
+    name: "Vapour",
+    hex: "#f0ffff",
+  },
+  {
+    name: "Sodium Silver",
+    hex: "#fffcee",
+  },
+  {
+    name: "Eyeball",
+    hex: "#fffbf8",
+  },
+  {
+    name: "Glamour White",
+    hex: "#fffcec",
+  },
+  {
+    name: "Aphrodite’s Pearls",
+    hex: "#eeffff",
+  },
+  {
+    name: "Cool December",
+    hex: "#fdfbf8",
+  },
+  {
+    name: "Heart of Ice",
+    hex: "#f7fcff",
+  },
+  {
+    name: "Deutzia White",
+    hex: "#f7fcfe",
+  },
+  {
+    name: "Vanilla Shake",
+    hex: "#fffbf0",
+  },
+  {
+    name: "Snow",
+    hex: "#fffafa",
+  },
+  {
+    name: "Whiteout",
+    hex: "#fbfbfb",
+  },
+  {
+    name: "Wash Me",
+    hex: "#fafbfd",
+  },
+  {
+    name: "Snowman",
+    hex: "#fefafb",
+  },
+  {
+    name: "Azure Mist",
+    hex: "#f0fff1",
+  },
+  {
+    name: "Honeydew",
+    hex: "#f0fff0",
+  },
+  {
+    name: "Floral White",
+    hex: "#fffaf0",
+  },
+  {
+    name: "Snow White",
+    hex: "#eeffee",
+  },
+  {
+    name: "Placebo Yellow",
+    hex: "#fcfbeb",
+  },
+  {
+    name: "Tuberose",
+    hex: "#fffaec",
+  },
+  {
+    name: "White Desert",
+    hex: "#fdfaf1",
+  },
+  {
+    name: "Sugar Milk",
+    hex: "#fff9f5",
+  },
+  {
+    name: "Dr. White",
+    hex: "#fafafa",
+  },
+  {
+    name: "Placebo Lime",
+    hex: "#f6fceb",
+  },
+  {
+    name: "Day Lily",
+    hex: "#fff9ec",
+  },
+  {
+    name: "Toasted Marshmallow Fluff",
+    hex: "#fff9eb",
+  },
+  {
+    name: "Amnesiac White",
+    hex: "#f8fbeb",
+  },
+  {
+    name: "Lime Slice",
+    hex: "#f0fded",
+  },
+  {
+    name: "Cold Snow",
+    hex: "#fff7fd",
+  },
+  {
+    name: "Cloud Petal",
+    hex: "#faf9f8",
+  },
+  {
+    name: "Doctor",
+    hex: "#f9f9f9",
+  },
+  {
+    name: "Cold White",
+    hex: "#edfcfb",
+  },
+  {
+    name: "Satin Purse",
+    hex: "#fff8ee",
+  },
+  {
+    name: "Enoki",
+    hex: "#f8faee",
+  },
+  {
+    name: "Ghost White",
+    hex: "#f8f8ff",
+  },
+  {
+    name: "Whitest White",
+    hex: "#f8f9f5",
+  },
+  {
+    name: "Blanca Peak",
+    hex: "#f8f9f4",
+  },
+  {
+    name: "Orange Albedo",
+    hex: "#faf8f7",
+  },
+  {
+    name: "Peaceful Rain",
+    hex: "#f1fbf1",
+  },
+  {
+    name: "Zhēn Zhū Bái Pearl",
+    hex: "#f8f8f9",
+  },
+  {
+    name: "Fatback",
+    hex: "#fff7ed",
+  },
+  {
+    name: "Ultra Pure White",
+    hex: "#f8f8f3",
+  },
+  {
+    name: "Spring White",
+    hex: "#ecfcec",
+  },
+  {
+    name: "White Crest",
+    hex: "#f9f8ef",
+  },
+  {
+    name: "Pure White",
+    hex: "#f8f8f2",
+  },
+  {
+    name: "Winter Oasis",
+    hex: "#f2faed",
+  },
+  {
+    name: "Placebo Green",
+    hex: "#ebfcec",
+  },
+  {
+    name: "Pearl Necklace",
+    hex: "#fcf7eb",
+  },
+  {
+    name: "Milk Jug",
+    hex: "#faf7f0",
+  },
+  {
+    name: "Lynx White",
+    hex: "#f7f7f7",
+  },
+  {
+    name: "Alice Blue",
+    hex: "#f0f8ff",
+  },
+  {
+    name: "Mā White",
+    hex: "#f4f7fd",
+  },
+  {
+    name: "Van de Cane",
+    hex: "#faf7eb",
+  },
+  {
+    name: "Distant Horizon",
+    hex: "#f1f8fa",
+  },
+  {
+    name: "Night Blooming Jasmine",
+    hex: "#f9f7ec",
+  },
+  {
+    name: "Foggy Pith",
+    hex: "#fbf6ef",
+  },
+  {
+    name: "High Reflective White",
+    hex: "#f7f7f1",
+  },
+  {
+    name: "Dipped in Cream",
+    hex: "#fcf6eb",
+  },
+  {
+    name: "Walkie Chalkie",
+    hex: "#faf5fa",
+  },
+  {
+    name: "Cotton Ball",
+    hex: "#f2f7fd",
+  },
+  {
+    name: "Laundry White",
+    hex: "#f6f7f1",
+  },
+  {
+    name: "Coy",
+    hex: "#fff4f3",
+  },
+  {
+    name: "Pink Diminishing",
+    hex: "#fff4f2",
+  },
+  {
+    name: "Praxeti White",
+    hex: "#f6f7ed",
+  },
+  {
+    name: "Silver Bird",
+    hex: "#fbf5f0",
+  },
+  {
+    name: "Gram’s Hair",
+    hex: "#f5f6f7",
+  },
+  {
+    name: "Placebo Orange",
+    hex: "#fcf5eb",
+  },
+  {
+    name: "Ambrosia Ivory",
+    hex: "#fff4eb",
+  },
+  {
+    name: "Grass Valley",
+    hex: "#f4f7ee",
+  },
+  {
+    name: "Glimpse of Pink",
+    hex: "#fff3f4",
+  },
+  {
+    name: "Sugar Crystal",
+    hex: "#f8f4ff",
+  },
+  {
+    name: "Ultra White",
+    hex: "#f6f6f0",
+  },
+  {
+    name: "Cascading White",
+    hex: "#f7f5f6",
+  },
+  {
+    name: "Soft Peach Mist",
+    hex: "#fff3f0",
+  },
+  {
+    name: "Silent Snowfall",
+    hex: "#eef7fa",
+  },
+  {
+    name: "Venerable White",
+    hex: "#f7f5f3",
+  },
+  {
+    name: "White Smoke",
+    hex: "#f5f5f5",
+  },
+  {
+    name: "Angraecum Orchid",
+    hex: "#f4f6ec",
+  },
+  {
+    name: "Snow Day",
+    hex: "#f7f5ed",
+  },
+  {
+    name: "Foamy Milk",
+    hex: "#f7f4f7",
+  },
+  {
+    name: "Icy Tundra",
+    hex: "#f7f5ec",
+  },
+  {
+    name: "Pinkish White",
+    hex: "#fff1fa",
+  },
+  {
+    name: "Soft Petals",
+    hex: "#ebf8ef",
+  },
+  {
+    name: "Polished Pink",
+    hex: "#fff2ef",
+  },
+  {
+    name: "Ivory Tower",
+    hex: "#fbf3f1",
+  },
+  {
+    name: "French Porcelain",
+    hex: "#f6f4f6",
+  },
+  {
+    name: "Cultured",
+    hex: "#f6f4f5",
+  },
+  {
+    name: "Crystal Cut",
+    hex: "#f8f4ed",
+  },
+  {
+    name: "White Chalk",
+    hex: "#f6f4f1",
+  },
+  {
+    name: "The Speed of Light",
+    hex: "#f6f4ef",
+  },
+  {
+    name: "Sefid White",
+    hex: "#fff1f1",
+  },
+  {
+    name: "Decorator White",
+    hex: "#f6f4ec",
+  },
+  {
+    name: "Chef’s Hat",
+    hex: "#f3f4f5",
+  },
+  {
+    name: "Indigo White",
+    hex: "#ebf6f7",
+  },
+  {
+    name: "Morning Snow",
+    hex: "#f5f4ed",
+  },
+  {
+    name: "Classic Chalk",
+    hex: "#f4f4f0",
+  },
+  {
+    name: "Lavender Blush",
+    hex: "#fff0f5",
+  },
+  {
+    name: "White Owl",
+    hex: "#f5f3f5",
+  },
+  {
+    name: "Heavy Sugar",
+    hex: "#eff5f1",
+  },
+  {
+    name: "Liquid Nitrogen",
+    hex: "#f3f3f4",
+  },
+  {
+    name: "Cloud Break",
+    hex: "#f6f1fe",
+  },
+  {
+    name: "Boysenberry Shadow",
+    hex: "#f1f3f9",
+  },
+  {
+    name: "Bakery Box",
+    hex: "#f0f4f2",
+  },
+  {
+    name: "Lovely Euphoric Delight",
+    hex: "#ffeeff",
+  },
+  {
+    name: "Bleached Silk",
+    hex: "#f3f3f2",
+  },
+  {
+    name: "Powder Puff",
+    hex: "#ffeff3",
+  },
+  {
+    name: "Bright White",
+    hex: "#f6f2f1",
+  },
+  {
+    name: "Placebo Blue",
+    hex: "#ebf4fc",
+  },
+  {
+    name: "Zappy Zebra",
+    hex: "#f1f3f3",
+  },
+  {
+    name: "White Convolvulus",
+    hex: "#f4f2f4",
+  },
+  {
+    name: "Shiroi White",
+    hex: "#ebf5f0",
+  },
+  {
+    name: "Hotot Bunny",
+    hex: "#f1f3f2",
+  },
+  {
+    name: "Grim White",
+    hex: "#f6f1f4",
+  },
+  {
+    name: "Pale Cherry Blossom",
+    hex: "#fdeff2",
+  },
+  {
+    name: "Alpine Goat",
+    hex: "#f1f2f8",
+  },
+  {
+    name: "Tell Me a Secret",
+    hex: "#f0f3f1",
+  },
+  {
+    name: "Errigal White",
+    hex: "#f2f2f4",
+  },
+  {
+    name: "Bit of Sugar",
+    hex: "#f4f2ec",
+  },
+  {
+    name: "Insignia White",
+    hex: "#ecf3f9",
+  },
+  {
+    name: "Apple Flower",
+    hex: "#edf4eb",
+  },
+  {
+    name: "Pearl White",
+    hex: "#f3f2ed",
+  },
+  {
+    name: "Partial Pink",
+    hex: "#ffedf8",
+  },
+  {
+    name: "White Gloss",
+    hex: "#ffeeee",
+  },
+  {
+    name: "Aragonite White",
+    hex: "#f3f1f3",
+  },
+  {
+    name: "Delicate White",
+    hex: "#f1f2ee",
+  },
+  {
+    name: "Voracious White",
+    hex: "#feeeed",
+  },
+  {
+    name: "Airport",
+    hex: "#edf2f8",
+  },
+  {
+    name: "Beluga",
+    hex: "#eff2f1",
+  },
+  {
+    name: "Lustre White",
+    hex: "#f4f1ec",
+  },
+  {
+    name: "Maui Mist",
+    hex: "#eef2f3",
+  },
+  {
+    name: "Pearl Sugar",
+    hex: "#f4f1eb",
+  },
+  {
+    name: "Plastic Clouds",
+    hex: "#f5f0f1",
+  },
+  {
+    name: "Translucent Unicorn",
+    hex: "#ffedef",
+  },
+  {
+    name: "Soft Breeze",
+    hex: "#f6f0eb",
+  },
+  {
+    name: "Leukocyte White",
+    hex: "#f2f1ed",
+  },
+  {
+    name: "Vienna Dawn",
+    hex: "#f7efef",
+  },
+  {
+    name: "Sentimental Pink",
+    hex: "#f8eef4",
+  },
+  {
+    name: "Isabelline",
+    hex: "#f4f0ec",
+  },
+  {
+    name: "Strawberry Bonbon",
+    hex: "#ffebfa",
+  },
+  {
+    name: "Mineral Spring",
+    hex: "#edf2ec",
+  },
+  {
+    name: "Cascade White",
+    hex: "#ecf2ec",
+  },
+  {
+    name: "Precious Pearls",
+    hex: "#f1f0ef",
+  },
+  {
+    name: "Powder White",
+    hex: "#ebf1f5",
+  },
+  {
+    name: "Snowbelt",
+    hex: "#eef1ec",
+  },
+  {
+    name: "Furry Lady",
+    hex: "#f5efeb",
+  },
+  {
+    name: "Angelic Wings",
+    hex: "#f4efee",
+  },
+  {
+    name: "Little Pinky",
+    hex: "#f4efed",
+  },
+  {
+    name: "White Shoulders",
+    hex: "#f1f0ec",
+  },
+  {
+    name: "Placebo Pink",
+    hex: "#fcebfa",
+  },
+  {
+    name: "Snowflake",
+    hex: "#eff0f0",
+  },
+  {
+    name: "Grannie’s Pearls",
+    hex: "#f6eeed",
+  },
+  {
+    name: "Just Pink Enough",
+    hex: "#ffebee",
+  },
+  {
+    name: "Foundation White",
+    hex: "#efeeff",
+  },
+  {
+    name: "Placebo Magenta",
+    hex: "#fcebf4",
+  },
+  {
+    name: "Carte Blanche",
+    hex: "#eeeeff",
+  },
+  {
+    name: "Placebo Fuchsia",
+    hex: "#f8ebfc",
+  },
+  {
+    name: "White Glove",
+    hex: "#f0efed",
+  },
+  {
+    name: "White Picket Fence",
+    hex: "#f0efeb",
+  },
+  {
+    name: "Placebo Red",
+    hex: "#fcebeb",
+  },
+  {
+    name: "Line Dried Sheets",
+    hex: "#f5eded",
+  },
+  {
+    name: "First Blush",
+    hex: "#f4edec",
+  },
+  {
+    name: "Snowy Mount",
+    hex: "#f1eeeb",
+  },
+  {
+    name: "Perfect White",
+    hex: "#f0eeee",
+  },
+  {
+    name: "Bái Sè White",
+    hex: "#ecefef",
+  },
+  {
+    name: "Magical Moonlight",
+    hex: "#f0eeeb",
+  },
+  {
+    name: "Vintage Porcelain",
+    hex: "#f2edec",
+  },
+  {
+    name: "Super Silver",
+    hex: "#eeeeee",
+  },
+  {
+    name: "White Whale",
+    hex: "#edeeef",
+  },
+  {
+    name: "Springtime Rain",
+    hex: "#ebeef3",
+  },
+  {
+    name: "Clichy White",
+    hex: "#f6ebee",
+  },
+  {
+    name: "Dress Pink",
+    hex: "#f4ebef",
+  },
+  {
+    name: "White Edgar",
+    hex: "#ededed",
+  },
+  {
+    name: "Violet Clues",
+    hex: "#efecef",
+  },
+  {
+    name: "Sheikh White",
+    hex: "#efecee",
+  },
+  {
+    name: "Snowberry",
+    hex: "#efeced",
+  },
+  {
+    name: "Tropical Breeze",
+    hex: "#ebedee",
+  },
+  {
+    name: "Coronation",
+    hex: "#edecec",
+  },
+  {
+    name: "Bright Grey",
+    hex: "#ebecf0",
+  },
+  {
+    name: "Mercury",
+    hex: "#ebebeb",
+  },
+] as const;
